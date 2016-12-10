eca47846632e3b3656ac1ae2d166bf4d00f8260b
Evan Paige <ewpaige@gmail.com>
2016-12-10 10:43:44 -0800

    cleaned up the file-reader and pushed our information into the object

--- lab-evan-erick-michael/lib/bmp-reader.js
+++ lab-evan-erick-michael/lib/bmp-reader.js
@@ -2,26 +2,16 @@
 
 const fs = require('fs');
 const bitmap = fs.readFileSync(`../img/palette-bitmap.bmp`);
+var bmp = require('../model/bitmap-constructor.js');
+console.log(bmp);
 
-const bmp = {};
+var ourBmp = new bmp(
+  bitmap.toString('utf-8', 0, 2),
+  bitmap.readInt32LE(2),
+  bitmap.readInt32LE(18),
+  bitmap.readInt32LE(22),
+  bitmap.readInt32LE(10),
+  bitmap.readInt32LE(46)
+);
 
-bmp.type = bitmap.toString('utf-8', 0, 2);
-
-// there are other options for this
-bmp.size = bitmap.readInt32LE(2);
-bmp.width = bitmap.readUInt32LE(18);
-bmp.height = bitmap.readUInt32LE(22);
-bmp.pixStart = bitmap.readUInt32LE(10);
-
-bmp.colorPallete = bitmap.readUInt32LE(46);
-// bmp.colorPallete = bitmap.readUInt32LE(54);
-// console.log(bmp.colorPallete);
-// console.log(bmp);
-// console.log(bmp.pixStart);
-var colorTable = new Buffer(bmp.colorPallete, 1077, 'hex');
-var pixArray = new Buffer(bmp.pixStart, 11078, 'hex');
-console.log('pixArray', pixArray);
-console.log('colorTable', colorTable);
-
-// fs.writeFile('./writeable.txt', pixArray, 'base64');
-// fs.writeFile('./writeable2.txt', colorTable, 'base64');
+console.log(ourBmp);
