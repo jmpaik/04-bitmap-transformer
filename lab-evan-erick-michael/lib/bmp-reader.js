ba1411b73583ff61fe717ee2175209f20024c529
Evan Paige <ewpaige@gmail.com>
2016-12-09 20:48:02 -0800

    adding changes from bmp-reader

--- lab-evan-erick-michael/lib/bmp-reader.js
+++ lab-evan-erick-michael/lib/bmp-reader.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const fs = require('fs');
-const bitmap = fs.readFileSync(`${__dirname}/assets/dummy-bmp.bmp`);
+const bitmap = fs.readFileSync(`../img/palette-bitmap.bmp`);
 
 const bmp = {};
 
@@ -11,5 +11,16 @@ bmp.type = bitmap.toString('utf-8', 0, 2);
 bmp.size = bitmap.readInt32LE(2);
 bmp.width = bitmap.readUInt32LE(18);
 bmp.height = bitmap.readUInt32LE(22);
+bmp.pixStart = bitmap.readUInt32LE(10);
+bmp.colorPallete = bitmap.readUInt32LE(46);
+// bmp.colorPallete = bitmap.readUInt32LE(54);
+// console.log(bmp.colorPallete);
+// console.log(bmp);
+// console.log(bmp.pixStart);
+var colorTable = new Buffer(bmp.colorPallete, 1077, 'hex');
+var pixArray = new Buffer(bmp.pixStart, 11078, 'hex');
+console.log('pixArray', pixArray);
+console.log('colorTable', colorTable);
 
-console.dir(bmp);
+// fs.writeFile('./writeable.txt', pixArray, 'base64');
+// fs.writeFile('./writeable2.txt', colorTable, 'base64');
