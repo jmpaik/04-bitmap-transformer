e204f0afec158da5def81f891a2fb9b9314aff8a
JINHO PAIK <JMPAIK83@GMAIL.COM>
2016-12-11 10:25:03 -0800

    read Palette function

--- lab-evan-erick-michael/lib/bmp-reader.js
+++ lab-evan-erick-michael/lib/bmp-reader.js
@@ -2,22 +2,48 @@
 
 const fs = require('fs');
 
-const bitmap = fs.readFileSync(`${__dirname}../img/palette-bmp.bmp`);
+//open file with fs and read file into buffer
+module.exports = fs.readFile(`${__dirname}../img/palette-bmp.bmp`, (err, bufferData) => {
+  if (err) {
+    console.log('error', err);
+  }
+// const bitmap = fs.readFileSync(`${__dirname}../img/palette-bmp.bmp`);
+// const bitmap = fs.readFileSync(`../img/palette-bitmap.bmp`);
+// var bmp = require('../model/bitmap-constructor.js');
+//
+// console.log(bmp);
 
-const bitmap = fs.readFileSync(`../img/palette-bitmap.bmp`);
-var bmp = require('../model/bitmap-constructor.js');
-console.log(bmp);
-
-var ourBmp = new bmp(
-  bitmap.toString('utf-8', 0, 2),
-  bitmap.readInt32LE(2),
-  bitmap.readInt32LE(18),
-  bitmap.readInt32LE(22),
-  bitmap.readInt32LE(10),
-  bitmap.readInt32LE(46)
-);
+//convert buffer into JS Object
+var bitmap = {};
+  bitmap.size = bufferData.readInt32LE(2);
+  bitmap.pixelStart = bufferData.readInt32LE(10);
+  bitmap.dBISize = bufferData.readInt32LE(12);
+  bitmap.colorPalette = bufferData.readInt32LE(54);
+  // bitmap.toString('utf-8', 0, 2),
+  // bitmap.readInt32LE(2),
+  // bitmap.readInt32LE(18),
+  // bitmap.readInt32LE(22),
+  // bitmap.readInt32LE(10),
+  // bitmap.readInt32LE(46)
 
 // console.log(ourBmp);
+
+//read color palette into integer array
+ var palette = [];
+
+ bitmap.readPalette = function() {
+   var counter = 0
+   for (var i = 54; i < bitmap.pixelStart; i += 4) {
+     palette[counter] = [
+       bufferData.readUInt8(i),
+       bufferData.readUInt8(i + 1),
+       bufferData.readUInt8(i + 2),
+       0];
+       counter++;
+   }
+ };
+ //call readPalette function? x-fingers.
+ bitmap.readPalette();
 
 // notice that logging the length of the pallette gives us 256... leads
 // me to believe that each 2 chars represents a color in the pallette
@@ -27,12 +53,12 @@ var ourBmp = new bmp(
 // I'll keep digging
 
 var createGroupedArray = function(name, arr, chunkSize, newArray) {
-    var groups = [], i;
-    for (i = 0; i < arr.length; i += chunkSize) {
-        groups.push(arr.slice(i, i + chunkSize));
-    }
-    newArray.push(groups);
-    console.log(name, newArray);
-}
+  var groups = [], i;
+  for (i = 0; i < arr.length; i += chunkSize) {
+    groups.push(arr.slice(i, i + chunkSize));
+  }
+  newArray.push(groups);
+  console.log(name, newArray);
+};
 createGroupedArray('colors', ourBmp.colorTable, 4, ourBmp.ourColors);
 createGroupedArray('pixArrayBrokenDown', ourBmp.pixArray, 4, ourBmp.pixArrayBrokenDown);
