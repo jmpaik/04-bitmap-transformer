e21c91d3342ca21739bddf41540fefdc038ad69d
kcirekcom <erick.f.mock@gmail.com>
2016-12-11 17:45:29 -0800

    created inversion transformer

--- lab-evan-erick-michael/lib/bmp-reader.js
+++ lab-evan-erick-michael/lib/bmp-reader.js
@@ -31,8 +31,9 @@ module.exports = fs.readFile('../img/palette-bitmap.bmp', (err, bufferData) => {
 
 //read color palette into integer array
   var palette = [];
+  var invertedPalette = [];
 
-  bitmap.readPalette = function() {
+  bitmap.readPalette = function(palette) {
     var counter = 0;
     for (var i = 54; i < bitmap.pixelStart; i += 4) {
       palette[counter] = [
@@ -43,8 +44,10 @@ module.exports = fs.readFile('../img/palette-bitmap.bmp', (err, bufferData) => {
       counter++;
     }
   };
+
  //call readPalette function? x-fingers.
-  bitmap.readPalette();
+  bitmap.readPalette(palette);
+  bitmap.readPalette(invertedPalette);
 
  //manipulate values to transform palette for rgb to change colors in palette
   palette.transform = function(data) {
@@ -58,9 +61,27 @@ module.exports = fs.readFile('../img/palette-bitmap.bmp', (err, bufferData) => {
     return data;
   };
 
+  invertedPalette.transform = function(data) {
+    for (var i = 0; i < data.length; i++) {
+      var pixelPalette = data[i];
+      var red = 255 - pixelPalette[0];
+      var green = 255 - pixelPalette[1];
+      var blue = 255 - pixelPalette[2];
+
+      data[i] = [red, green, blue, 0];
+    }
+    return data;
+  };
+
+
+
+
+
 //call transform function, store array in variable, make one array with all nested arrays
   var transformedPalette = palette.transform(palette);
+  var transformedPaletteInverted = invertedPalette.transform(invertedPalette);
   var allArray = [];
+  var allArrayTwo = [];
 
   transformedPalette.forEach(function(item) {
     item.forEach(function(item2) {
@@ -68,14 +89,28 @@ module.exports = fs.readFile('../img/palette-bitmap.bmp', (err, bufferData) => {
     });
   });
 
+  transformedPaletteInverted.forEach(function(item) {
+    item.forEach(function(item2) {
+      allArrayTwo.push(item2);
+    });
+  });
+
 //create new buffer of transformed color palette
   var paletteBuffer = new Buffer(allArray);
+  var paletteInvertedBuffer = new Buffer(allArrayTwo);
   var header = bufferData.slice(0, 54);
   var tail = bufferData.slice(1078);
   //concat header, transform buffer and tail to read to file
   var newBuffer = Buffer.concat([header, paletteBuffer, tail]);
+  var newBufferTwo = Buffer.concat([header, paletteInvertedBuffer, tail]);
   //write new buffer to file
   fs.writeFile('../img/palette-bmp.bmp', newBuffer, (err) => {
+    if (err) {
+      console.log('error', err);
+    }
+  });
+
+  fs.writeFile('../img/palette-inverted.bmp', newBufferTwo, (err) => {
     if (err) {
       console.log('error', err);
     }
