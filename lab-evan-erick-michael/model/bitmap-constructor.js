7ac23d8d0e02a0d38b9587429501db62228326d0
JINHO PAIK <JMPAIK83@GMAIL.COM>
2016-12-12 11:57:27 -0800

    building constructors

--- lab-evan-erick-michael/model/bitmap-constructor.js
+++ lab-evan-erick-michael/model/bitmap-constructor.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var BitmapConstrutor = module.exports = function(type, size, pixelStart, dBISize, colorPalette) {
+var BitmapConstructor = module.exports = function(type, size, pixelStart, dBISize, colorPalette) {
   this.type = type;
   this.size = size;
   this.pixelStart = pixelStart;
@@ -9,6 +9,7 @@ var BitmapConstrutor = module.exports = function(type, size, pixelStart, dBISize
   this.grayPalette = [];
   this.randomPalette = [];
   this.invertedPalette = [];
+  this.scaledPalette = [];
   this.getMaxVal = function() {
     var counter = 0;
     for (var i = this.colorPalette; i < this.pixelStart; i += 4) {
@@ -21,10 +22,10 @@ var BitmapConstrutor = module.exports = function(type, size, pixelStart, dBISize
       counter++;
     }
   };
-  this.readPalette = function() {
+  this.readPalette = function(palette) {
     var counter = 0;
     for (var i = 54; i < bitmap.pixelStart; i += 4) {
-      this.randomPalette[counter] = [
+      palette[counter] = [
         bufferData.readUInt8(i),
         bufferData.readUInt8(i + 1),
         bufferData.readUInt8(i + 2),
@@ -32,32 +33,4 @@ var BitmapConstrutor = module.exports = function(type, size, pixelStart, dBISize
       counter++;
     }
   };
-
-  this.transformGray = function(data) {
-    for (var i = 0; i < data.length; i++) {
-      let red = this.grayPalette[i];
-      let green = this.grayPalette[i];
-      let blue = this.grayPalette[i];
-
-      data[i] = [red, green, blue, 0];
-    }
-    console.log(data);
-    return data;
-  };
-  this.randomTransform = function(data) {
-    for (var i = 0; i < data.length; i++) {
-      let red = Math.floor(Math.random() * 255);
-      let green = Math.floor(Math.random() * 255);
-      let blue = Math.floor(Math.random() * 255);
-
-      data[i] = [red, green, blue, 0];
-    }
-    console.log(data);
-    return data;
-  };
-
-  this.transformedGrayPalette = this.transformGray(this.grayPalette);
-  this.transformedRandomPalette = this.randomTransform(this.randomPalette);
-  this.wholeArray = [];
-
 };
