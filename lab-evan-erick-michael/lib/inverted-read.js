04d14a08f10e299f570bde615fb3f85c5858ee96
kcirekcom <erick.f.mock@gmail.com>
2016-12-12 09:58:04 -0800

    created separate inverted read

--- lab-evan-erick-michael/lib/inverted-read.js
+++ lab-evan-erick-michael/lib/inverted-read.js
@@ -16,54 +16,49 @@ module.exports = fs.readFile('../img/palette-bitmap.bmp', (err, bufferData) => {
   bitmap.colorPalette = bufferData.readInt32LE(54);
 
 //read color palette into integer array
-  var palette = [];
-
-  bitmap.readPalette = function(palette) {
-    var counter = 0;
-    for (var i = 54; i < bitmap.pixelStart; i += 4) {
-      palette[counter] = [
-        bufferData.readUInt8(i),
-        bufferData.readUInt8(i + 1),
-        bufferData.readUInt8(i + 2),
-        0];
-      counter++;
-    }
-  };
+  var invertedPalette = [];
 
  //call readPalette function? x-fingers.
-  bitmap.readPalette(palette);
+  bitmap.readPalette(invertedPalette);
 
  //manipulate values to transform palette for rgb to change colors in palette
-  palette.transform = function(data) {
+
+  invertedPalette.transform = function(data) {
     for (var i = 0; i < data.length; i++) {
-      var red = Math.floor(Math.random() * 255);
-      var green = Math.floor(Math.random() * 255);
-      var blue = Math.floor(Math.random() * 255);
+      let pixelPalette = data[i];
+      let red = 255 - pixelPalette[0];
+      let green = 255 - pixelPalette[1];
+      let blue = 255 - pixelPalette[2];
 
       data[i] = [red, green, blue, 0];
     }
     return data;
   };
 
+
+
+
+
 //call transform function, store array in variable, make one array with all nested arrays
-  var transformedPalette = palette.transform(palette);
+  var transformedPaletteInverted = invertedPalette.transform(invertedPalette);
   var allArray = [];
 
-  transformedPalette.forEach(function(item) {
+  transformedPaletteInverted.forEach(function(item) {
     item.forEach(function(item2) {
       allArray.push(item2);
     });
   });
 
 //create new buffer of transformed color palette
-  var paletteBuffer = new Buffer(allArray);
+  var paletteInvertedBuffer = new Buffer(allArray);
   var header = bufferData.slice(0, 54);
   var tail = bufferData.slice(1078);
   //concat header, transform buffer and tail to read to file
-  var newBuffer = Buffer.concat([header, paletteBuffer, tail]);
+  var newBuffer = Buffer.concat([header, paletteInvertedBuffer, tail]);
   //write new buffer to file
-  fs.writeFile('../img/palette-bmp.bmp', newBuffer, (err) => {
+  fs.writeFile('../img/palette-inverted.bmp', newBuffer, (err) => {
     if (err) {
       console.log('error', err);
     }
   });
+});
